a3dffab4edfad63aa592e871bc2fb3a4098e4315
Dario Cavuotti <dario.cavuotti@tiendanube.com>
2020-05-10 16:30:18 -0300

    Update aliases

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,21 +1,23 @@
 git-prompt-alias() {
-    alias add="git add " \
-        push="git push -u origin " \
+    alias add="git add" \
+        push="git push -u origin" \
         s="git status" \
         status="git status" \
         co="git checkout" \
-        branch="git checkout -b " \
-        commit="git commit " \
-        com="git commit " \
-        d="git diff " \
-        show="git show " \
+        branch="git checkout -b" \
+        commit="git commit" \
+        com="git commit" \
+        d="git diff" \
+        show="git show" \
         up="git pull" \
+        pull="git pull" \
         reflog="git reflog" \
-        stash="git stash "
+        stash="git stash"
+        remote="git remote"
 }
 
 git-prompt-unalias() {
     unalias add push s status co \
-        branch commit com d show up \
-        reflog stash
+        branch commit com d show up pull \
+        reflog stash remote
 }
